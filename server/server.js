4c5969602b9536f33eaaf419598eb31fb3998986
Aaron Brightman <aaronbrightman1@gmail.com>
2017-06-22 12:27:49 -0400

    Mongoose installed and Simple Server with a Mongoose-Model Setup

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+//Telling Mongoose what promise library to use, promise behavior
+// history: Promises were originally part of the library called Blue Bird, was later added to JS language
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+
+//mongoose needs 'model' structure to save to collection
+//  // Second argument here is the Schema/Object
+var Todo = mongoose.model('Todo',{
+    text : {
+        type:String
+    },
+    completed : {
+        type : Boolean
+    },
+    completedAt :
+    {
+        type : Number
+    }
+});
+
+
+// Now can instatiate a model
+var newTodo = new Todo({
+    text : 'Go out to dinner'
+});
+
+
+// thats all
+newTodo.save()
+.then(document=>{
+    console.log(document)
+},err=>{console.log(err)});
+
