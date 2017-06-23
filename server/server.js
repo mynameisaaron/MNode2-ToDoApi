b235e7c45bd5148f34d3daaf10b86427dd5a67f6
Aaron Brightman <aaronbrightman1@gmail.com>
2017-06-23 09:10:16 -0400

    Post Endpoint added to Todos

--- server/server.js
+++ server/server.js
@@ -1,5 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
 var {User} = require('./models/User');
 
-// ALL REFACTORED, READY TO CODE BELOW
+
+var app = express();
+
+/////////////////////////////////////
+/////////////////////////////////////
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res,next)=>{
+    
+    var newTodo = new Todo({
+        text:req.body.text
+    });
+
+    newTodo.save()
+    .then(document=>res.status(201).send(document))
+    .catch(e=>res.status(400).send(e));
+});
+
+/////////////////////////////////////
+/////////////////////////////////////
+var ListeningPort = 3000;
+app.listen(ListeningPort,()=>{
+    console.log('You are connected to port '+ ListeningPort);
+});
+
