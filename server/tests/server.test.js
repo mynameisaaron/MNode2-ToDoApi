d33fdd97d016f60d906bb0ade697e4b224893136
Aaron Brightman <aaronbrightman1@gmail.com>
2017-06-23 11:19:32 -0400

    Implemented POST endpoint test AND setup Scripts

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,10 +1,62 @@
 const expect = require('expect');
 const request = require('supertest');
 
-//NOTICE THAT MOCHA DOES NOT WORK IN THE WAY THAT IT NEEDS TO BE REQUIRED
-//NOTICE THAT SUPERTEST IS ASSIGNED TO REQUEST
+const { app } = require('../server');
+const { Todo } = require('../models/Todo');
 
-////
-// NOW WE NEED ACCESS TO EXPRESS 'APP' OBJECT & TODO MODEL
-const {app} = require('../server');
-const {Todo} = require('../models/Todo');
+//MOCHA/SUPERTEST TESTING 
+
+///////////////////////
+beforeEach(done => {
+    Todo.remove({})
+        .then(() => done())
+});
+//////////////////////
+
+describe('Testing the POST - Todos endpoint', () => {
+
+
+    it('Should create a new Todo', done => {
+
+        var text = 'test new todo';
+        //supertest converts auto-converts sended object into json
+        request(app)
+            .post('/todos')
+            .send({ text })
+            .expect(201)
+            .expect(res => {
+                expect(res.body.text).toBe(text);
+            })
+            .end((err, res) => {
+
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.find()
+                    .then(todos => {
+                        expect(todos.length).toBe(1);
+                        expect(todos[0].text).toBe(text);
+                        done();
+                    })
+                    .catch(e => done(e));
+
+
+
+            });
+
+
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
