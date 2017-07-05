48f15b7f1e96c0510dce60307eda735b81e94803
Aaron Brightman <aaronbrightman1@gmail.com>
2017-07-05 09:23:19 -0400

    Set up the Authenticate Endpoint

--- server/models/User.js
+++ server/models/User.js
@@ -1,89 +1,104 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 
-// We want to 'tack-on' some custom methods to the model
-//   SO > we need to user the mongoose.Schema constructor method to create mongoose model
-//       , in order to add functions/methods to the model
-
 
 var UserSchema = new mongoose.Schema({
-    email : {
-        type : String,
-        required : true,
-        trim : true,
-        minlength : 1,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
         //validate w/ validator & message property - part of mongoosejs custom validation
         //validate function will return true if validated
         // 
         //FOLLOW THIS STRUCTURE
-        validate:{
-            validator : function(value)
-            {
-               return validator.isEmail(value); //=> true
+        validate: {
+            validator: function (value) {
+                return validator.isEmail(value); //=> true
             },
             message: "I'm sorry that is not a valid email"
         }
     },
-    password : {
-        type:String,
-        require:true,
-        minlength:6
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
     },
-    tokens:[{
-        
-            access:{
-                type:String,
-                required:true
-            }
-        
+    tokens: [{
+
+        access: {
+            type: String,
+            required: true
+        }
+
         ,
-        
-            token : {
-                type:String,
-                required:true
-             }
-        
-        
+
+        token: {
+            type: String,
+            required: true
+        }
+
+
     }]
 })
+///////////////////////////////////////////////////////////////////////////////
+// HERE WE ARE GOING TO MAKE A STATIC METHOD FOR USER, TO FIND BY TOKEN 
+
+// UserSchema.statics.findByToken = function (token) {
+//     var User = this;
+
+//     var UserId = jwt.verify(token, 'SaltString')._id;
+
+//    return User.findOne({_id:UserId});
 
 
-//HERE WE WANT TO OVERRIDE THE USER.TOJSON METHOD, TO LIMIT THE INFORMATION SETBACK FROM A POST
-// ONLY SENDS BACK EMAIL (NO PASSWORD OR TOKENS)
-UserSchema.methods.toJSON = function(){
+
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'SaltString');
+    } catch (e) {
+
+        //return new Promise((resolve,reject)=>{reject()});
+        // also can write this as
+        return Promise.reject();
+        
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
+
+
+
+
+////////////////////////////////////////////////////////
+
+UserSchema.methods.toJSON = function () {
     var user = this;
-
     var userObject = user.toObject();
-
-    return _.pick(userObject,['_id', 'email']);
+    return _.pick(userObject, ['_id', 'email']);
 }
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'SaltString').toString();
 
+    user.tokens.push({ access, token });
 
-
-// UserSchema.methods is an object, can add all the instance methods to objects here!
-        //SIDENOTE , Arrow functions 'do not bind the 'THIS' keyword'   We need to bind the 'this' keyword
-        //  NEED TO BIND 'THIS' TO THE INSTANCE OF THIS MODEL
-UserSchema.methods.generateAuthToken = function(){
-//ex of sidenote >   var user = this;
-var user = this;
-
-var access = 'auth';
-var token = jwt.sign({ _id:user._id.toHexString(), access}, 'SaltString').toString();
-
-user.tokens.push({access,token});
-
-//this is returning a promise with the token passed in
-return user.save().then(() => {
-     return token;
-   });
-// 'token' is passed in as the next success argument for the promise
-
+    return user.save().then(() => {
+        return token;
+    });
 }
 
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ var _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/Todo');
-var {User} = require('./models/User');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/Todo');
+var { User } = require('./models/User');
 
-var {ObjectID} = require('mongodb');
+var { ObjectID } = require('mongodb');
 
 var app = express();
 
@@ -16,60 +16,57 @@ var app = express();
 /////////////////////////////////////
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res,next)=>{
-    
+app.post('/todos', (req, res, next) => {
+
     var newTodo = new Todo({
-        text:req.body.text
+        text: req.body.text
     });
 
     newTodo.save()
-    .then(document=>res.status(201).send(document))
-    .catch(e=>res.status(400).send(e));
+        .then(document => res.status(201).send(document))
+        .catch(e => res.status(400).send(e));
 });
 
-app.get('/todos',(req,res,next)=>{
+app.get('/todos', (req, res, next) => {
     Todo.find()
-        .then(todos=>{res.send(todos)}, e=>{res.status(400).send(e)});
+        .then(todos => { res.send(todos) }, e => { res.status(400).send(e) });
 });
 
-app.get('/todos/:id',(req,res,next)=>{
+app.get('/todos/:id', (req, res, next) => {
     var TodoID = req.params.id;
 
     var isValid = ObjectID.isValid(TodoID);
 
-    if(!isValid)
-    {
+    if (!isValid) {
         res.status(404).send();
     }
-    
+
     Todo.findById(TodoID)
-    .then(singleObject=>{
-        if(!singleObject)
-        {
-           return res.status(404).send();
-        }
-        res.send(singleObject);
-    })
-    .catch(e=>res.status(400).send())
+        .then(singleObject => {
+            if (!singleObject) {
+                return res.status(404).send();
+            }
+            res.send(singleObject);
+        })
+        .catch(e => res.status(400).send())
 });
 
-app.delete('/todos/:id',(req,res,next)=>{
+app.delete('/todos/:id', (req, res, next) => {
     var _id = req.params.id;
 
-    if(!ObjectID.isValid(_id))
-    {
+    if (!ObjectID.isValid(_id)) {
         return res.status(404).send();
     }
 
     Todo.findByIdAndRemove(_id)
-    .then(document => {
-        if(!document){
-            return res.status(404).send();
-        }
+        .then(document => {
+            if (!document) {
+                return res.status(404).send();
+            }
 
-        res.send(document);
-    })
-    .catch(e => res.status(400).send());
+            res.send(document);
+        })
+        .catch(e => res.status(400).send());
 
 });
 
@@ -79,35 +76,32 @@ app.delete('/todos/:id',(req,res,next)=>{
 //  ALSO NOTICE that Todo.findByIdAndUpdate(_id,{$set:body},{new:true})
 //  {new:true} config object is the differnt way to return the new document ,, same result as {returnOriginal:false}
 //      
-app.patch('/todos/:id',(req,res,next)=>{
+app.patch('/todos/:id', (req, res, next) => {
     var _id = req.params.id;
-    var body = _.pick(req.body,['text','completed']);
+    var body = _.pick(req.body, ['text', 'completed']);
 
-    if(!ObjectID.isValid(_id))
-    {
+    if (!ObjectID.isValid(_id)) {
         return res.status(404).send();
     }
 
-    if(_.isBoolean(body.completed) && body.completed === true)
-    {
+    if (_.isBoolean(body.completed) && body.completed === true) {
         body.completedAt = new Date().getTime();
     }
-    else
-    {
+    else {
         body.completed = false;
         body.completedAt = null
     }
 
-    Todo.findByIdAndUpdate(_id,{$set:body},{new:true})
-    .then(newDocument=>{
+    Todo.findByIdAndUpdate(_id, { $set: body }, { new: true })
+        .then(newDocument => {
 
-        if(!newDocument){
-            return res.status(404).send();
-        }
+            if (!newDocument) {
+                return res.status(404).send();
+            }
 
-        return res.send(newDocument);
-    })
-    .catch(e => res.status(400).send());
+            return res.send(newDocument);
+        })
+        .catch(e => res.status(400).send());
 });
 
 
@@ -119,28 +113,39 @@ app.patch('/todos/:id',(req,res,next)=>{
 ///////////USERS API ENDPOINTS//////////
 ////////////////////////////////////////
 
-app.post('/users',(req,res,next)=>{
-    var body = _.pick(req.body,['email', 'password']);
+app.post('/users', (req, res, next) => {
+    var body = _.pick(req.body, ['email', 'password']);
 
     var user = new User(body);
     user.save()
-    .then(()=>{
-        //remember that when you call this method, its is directly on the object, not on the 'methods' property!
-       return user.generateAuthToken();
-    })
-    .then(token =>
-        res.status(201).header('x-auth',token).send(user)
-    )
-    .catch(e=>res.status(400).send(e));
+        .then(() => {
+            //remember that when you call this method, its is directly on the object, not on the 'methods' property!
+            return user.generateAuthToken();
+        })
+        .then(token =>
+            res.status(201).header('x-auth', token).send(user)
+        )
+        .catch(e => res.status(400).send(e));
+
+
+
+});
+/////////////////////////////////////////////
+
+app.get('/users/me', (req, res, next) => {
+
+    var authToken = req.header('x-auth');
+
+   User.findByToken(authToken)
+   .then(user =>{
+
+        
+       res.send(user);
+   })
+   .catch(e=>res.status(401).send());
+
     
     
-    // newUser.methods.generateAuthToken()
-    // .then(document=>res.status(201).send(document))
-    // .catch(e=>res.status(400).send(e));
-    
-    // newUser.save()
-    // .then(document=>res.status(201).send(document))
-    // .catch(e=>res.status(400).send(e));
 
 });
 
@@ -151,10 +156,10 @@ app.post('/users',(req,res,next)=>{
 
 
 var ListeningPort = process.env.PORT || 3000;
-app.listen(ListeningPort,()=>{
-    console.log('You are connected to port '+ ListeningPort);
+app.listen(ListeningPort, () => {
+    console.log('You are connected to port ' + ListeningPort);
 });
 
 
-module.exports = {app};
+module.exports = { app };
 
